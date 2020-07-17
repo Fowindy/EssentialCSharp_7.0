d527c405e450c9aa4bbc6790311710c9a41b6d3a
Fowindy <47002339@qq.com>
2020-07-18 06:01:16 +0800

    #Listing01_01.用CSharp编写的HelloWorld

---
+++ Src/Chapter01.CSharp概述/Listing01_01.用CSharp编写的HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年7月17日 星期五 23:11:48
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter01.CSharp概述.Listing01_01
+{
+    public class Listing01_01
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello.My name is Fowindy.");
+        }
+    }
+}
