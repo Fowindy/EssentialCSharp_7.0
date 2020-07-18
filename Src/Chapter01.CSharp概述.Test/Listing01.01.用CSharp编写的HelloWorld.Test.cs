bbe359d2b3324227722244fbb8884f58576b014f
Fowindy <47002339@qq.com>
2020-07-18 13:06:07 +0800

    #Chapter01.Tests.CSharp概述---测试项目完成

---
+++ Src/Chapter01.CSharp概述.Test/Listing01.01.用CSharp编写的HelloWorld.Test.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter01.Listing01_01
+{
+    [TestClass]
+    public class Listing01_01
+    {
+        [TestMethod]
+        public void Main_HelloWorld()
+        {
+            const string expected =
+                @"Hello.My name is Fowindy.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter01.CSharp概述.Listing01_01.Listing01_01.Main);
+        }
+    }
+}
