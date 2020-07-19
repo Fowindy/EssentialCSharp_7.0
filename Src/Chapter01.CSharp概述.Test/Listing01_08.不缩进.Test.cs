440b1aa44fa98305d4312af1128bc555a8c38709
Fowindy <47002339@qq.com>
2020-07-20 04:54:24 +0800

    #Listing01_08.不缩进+测试

---
+++ Src/Chapter01.CSharp概述.Test/Listing01_08.不缩进.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月20日 星期一 04:52:18
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月20日 星期一 04:52:18
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter01.CSharp概述.Test.Listing01_08
+{
+    [TestClass]
+    public class Listing01_08
+    {
+        [TestMethod]
+        public void Main_Listing01_08()
+        {
+            const string expected =
+                @"Hello,My name is Fowindy.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter01.CSharp概述.Listing01_08.Listing01_08.Main);
+        }
+    }
+}
---
+++ Src/Chapter01.CSharp概述/Listing01_08.不缩进.cs
@@ -0,0 +1,29 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月20日 星期一 04:48:59
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月20日 星期一 04:48:59
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter01.CSharp概述.Listing01_08
+{
+    public class Listing01_08
+    {
+    public static void Main()
+    {
+    Console.WriteLine("Hello,My name is Fowindy.");
+    }
+    }
+}
