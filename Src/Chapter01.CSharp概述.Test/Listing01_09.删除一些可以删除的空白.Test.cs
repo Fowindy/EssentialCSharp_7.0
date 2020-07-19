4122d4c1c7cb10b2c4a08ff98810419b0c5ae274
Fowindy <47002339@qq.com>
2020-07-20 04:59:17 +0800

    #Listing01_09.删除一些可以删除的空白+测试

---
+++ Src/Chapter01.CSharp概述.Test/Listing01_09.删除一些可以删除的空白.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月20日 星期一 04:58:15
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月20日 星期一 04:58:15
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter01.CSharp概述.Test.Listing01_09
+{
+    [TestClass]
+    public class Listing01_09
+    {
+        [TestMethod]
+        public void Main_Listing01_09()
+        {
+            const string expected =
+                @"Hello,My name is Fowindy.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter01.CSharp概述.Listing01_09.Listing01_09.Main);
+        }
+    }
+}
---
+++ Src/Chapter01.CSharp概述/Listing01_09.删除一些可以删除的空白.cs
@@ -0,0 +1,23 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月20日 星期一 04:55:46
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月20日 星期一 04:55:46
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter01.CSharp概述.Listing01_09
+{
+    public class Listing01_09{public static void Main(){Console.WriteLine("Hello,My name is Fowindy.");}}
+}
