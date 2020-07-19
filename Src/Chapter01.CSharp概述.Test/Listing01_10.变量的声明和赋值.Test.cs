4e1232158446d63723801a56d39727e368d703e3
Fowindy <47002339@qq.com>
2020-07-20 05:08:38 +0800

    #Listing01_10.变量的声明和赋值+测试

---
+++ Src/Chapter01.CSharp概述.Test/Listing01_10.变量的声明和赋值.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月20日 星期一 05:07:10
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月20日 星期一 05:07:10
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter01.CSharp概述.Test.Listing01_10
+{
+    [TestClass]
+    public class Listing01_10
+    {
+        [TestMethod]
+        public void Main_Listing01_10()
+        {
+            const string expected =
+                @"Have fun storming the castle!";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter01.CSharp概述.Listing01_10.Listing01_10.Main);
+        }
+    }
+}
---
+++ Src/Chapter01.CSharp概述/Listing01_10.变量的声明和赋值.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月20日 星期一 05:04:10
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月20日 星期一 05:04:10
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter01.CSharp概述.Listing01_10
+{
+    public class Listing01_10
+    {
+        public static void Main()
+        {
+            string max;     //string:数据类型;  max:变量
+            max = "Have fun storming the castle!";      //对变量进行赋值(初始化)
+            Console.WriteLine(max);     //对变量进行输出
+        }
+    }
+}
