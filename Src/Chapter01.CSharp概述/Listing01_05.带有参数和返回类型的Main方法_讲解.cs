29d8428c21dedebf95e4f5ac6ecb46df290f0c52
Fowindy <47002339@qq.com>
2020-07-19 19:21:29 +0800

    #Listing01_05.带有参数和返回类型的Main方法_讲解

---
+++ Src/Chapter01.CSharp概述/Listing01_05.带有参数和返回类型的Main方法_讲解.cs
@@ -0,0 +1,21 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年7月19日 星期日 19:16:10
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter01.CSharp概述.Listing01_05
+{
+    public class Listing01_05
+    {
+        public static int Main(string[] args)   //args参数是用于接收命令参数的字符串数组
+        {
+            //..
+            return 0;   //Main返回的是int状态码,标识程序执行成功与否,返回非0通常意味错误;
+        }
+    }
+}
