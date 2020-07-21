5fecf77f9f9833c4a5c78b7d4727b902f8ea3d6b
Fowindy <47002339@qq.com>
2020-07-21 14:50:48 +0800

    $变量的说明和书写格式

---
+++ Src/Chapter01.CSharp概述/Listing01_11.一条语句声明两个变量.cs
@@ -0,0 +1,30 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月21日 星期二 14:42:49
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月21日 星期二 14:42:49
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter01.CSharp概述.Listing01_11
+{
+    public class Listing01_11
+    {
+        public static void Main()
+        {
+            //一条语句声明两个string类型的变量
+            string message1, message2;
+        }
+    }
+}
