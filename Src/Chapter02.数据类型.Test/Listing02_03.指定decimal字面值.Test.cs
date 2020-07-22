ccd0c2686f15f9f22272dd09ec96e5b451f271c7
Fowindy <47002339@qq.com>
2020-07-22 18:15:49 +0800

    #Listing02_03.指定decimal字面值+测试+字面值解析顺序

---
+++ Src/Chapter02.数据类型.Test/Listing02_03.指定decimal字面值.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 18:13:51
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 18:13:51
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter02.数据类型.Test.Listing02_03
+{
+    [TestClass]
+    public class Listing02_03
+    {
+        [TestMethod]
+        public void Main_Listing02_03()
+        {
+            const string expected =
+                @"1618.033988749895";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter02.数据类型.Listing02_03.Listing02_03.Main);
+        }
+    }
+}
