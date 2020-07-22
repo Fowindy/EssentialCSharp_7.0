886b59ca30822de91753a9cbe3bc1611438682ef
Fowindy <47002339@qq.com>
2020-07-22 22:19:45 +0800

    #Listing02_05.指数记数法+测试

---
+++ Src/Chapter02.数据类型/Listing02_05.指数记数法.cs
@@ -0,0 +1,30 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 22:15:20
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 22:15:20
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter02.数据类型.Listing02_05
+{
+    public class Listing02_05
+    {
+        public static void Main()
+        {
+            Console.WriteLine(6.023E23F);
+            //输出 6.023E+23
+        }
+    }
+}
