837d84d0fa5a9002cee938d65324b378e8741030
Fowindy <47002339@qq.com>
2020-07-22 12:55:01 +0800

    #Listing01_18.交换索引占位符和对应变量+测试

---
+++ Src/Chapter01.CSharp概述.Test/Listing01_18.交换索引占位符和对应变量.Test.cs
@@ -0,0 +1,34 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 11:31:25
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 11:31:25
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter01.CSharp概述.Test.Listing01_18
+{
+    [TestClass]
+    public class Listing01_18
+    {
+        [TestMethod]
+        public void Main_Listing01_18()
+        {
+            const string expected =
+                @"Hey You!
+Please enter your first name:<<yue
+>>Please enter your last name:<<wang
+>>Your full name is wang yue.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter01.CSharp概述.Listing01_18.Listing01_18.Main);
+        }
+    }
+}
---
+++ Src/Chapter01.CSharp概述/Listing01_18.交换索引占位符和对应变量.cs
@@ -0,0 +1,34 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 11:24:26
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 11:24:26
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter01.CSharp概述.Listing01_18
+{
+    public class Listing01_18
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hey You!");
+            Console.Write("Please enter your first name:");
+            string firstName = Console.ReadLine();
+            Console.Write("Please enter your last name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Your full name is {1} {0}.", firstName,lastName);
+        }
+    }
+}
