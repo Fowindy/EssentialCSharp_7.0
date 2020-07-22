cc9239d3da96c7ae1f3e885969d58b6e8189120b
Fowindy <47002339@qq.com>
2020-07-22 09:35:31 +0800

    #Listing01_14.从控制台获取输入_ReadLine+测试

---
+++ Src/Chapter01.CSharp概述.Test/Listing01_14.从控制台获取输入_ReadLine.Test.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 09:12:43
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 09:12:43
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter01.CSharp概述.Test.Listing01_14
+{
+    [TestClass]
+    public class Listing01_14
+    {
+        [TestMethod]
+        public void Main_Listing01_14()
+        {
+            const string expected =
+@"Hey you!
+Please enter your first name:<<yue
+>>Please enter your last name:<<wang
+>>";
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter01.CSharp概述.Listing01_14.Listing01_14.Main);
+        }
+    }
+}
---
+++ Src/Chapter01.CSharp概述/Listing01_14.从控制台获取输入_ReadLine.cs
@@ -0,0 +1,35 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 09:08:10
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 09:08:10
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter01.CSharp概述.Listing01_14
+{
+    public class Listing01_14
+    {
+        public static void Main()
+        {
+            string firstName;
+            string lastName;
+            Console.WriteLine("Hey you!");
+            Console.Write("Please enter your first name:");
+            firstName = Console.ReadLine();
+            Console.Write("Please enter your last name:");
+            lastName = Console.ReadLine();
+        }
+    }
+}
