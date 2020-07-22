dca910dfd6917835a3be235310a90e6962740a2f
Fowindy <47002339@qq.com>
2020-07-22 17:40:20 +0800

    #Listing02_02.指定double字面值+测试

---
+++ Src/Chapter02.数据类型.Test/Listing02_02.指定double字面值.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 17:38:30
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 17:38:30
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter02.数据类型.Test.Listing02_02
+{
+    [TestClass]
+    public class Listing02_02
+    {
+        [TestMethod]
+        public void Main_Listing02_02()
+        {
+            const string expected =
+                @"1618.03398874989";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter02.数据类型.Listing02_02.Listing02_02.Main);
+        }
+    }
+}
---
+++ Src/Chapter02.数据类型/Listing02_02.指定double字面值.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 17:34:42
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 17:34:42
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter02.数据类型.Listing02_02
+{
+    public class Listing02_02
+    {
+        public static void Main()
+        {
+            Console.WriteLine(1618.033988749895);
+            //输出    1618.03398874989
+            //double精度为16位有效数字,所以最后数字5丢失了
+        }
+    }
+}
