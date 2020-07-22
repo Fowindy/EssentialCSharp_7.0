bc7be497a1988e4fa225fe229aff00aa54f8fc36
Fowindy <47002339@qq.com>
2020-07-22 11:08:09 +0800

    #Listing01_16.使用字符串插值_美元符_进行格式化+测试

---
+++ Src/Chapter01.CSharp概述.Test/Listing01_16.使用字符串插值_美元符_进行格式化.Test.cs
@@ -0,0 +1,36 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 11:04:59
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 11:04:59
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter01.CSharp概述.Test.Listing01_16
+{
+    [TestClass]
+    public class Listing01_16
+    {
+        [TestMethod]
+        public void Main_Listing01_16()
+        {
+            const string expected =
+                @"Hey You!
+Please enter your first name:
+<<yue
+>>Please enter your last name:
+<<wang
+>>Your full name is yue wang.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter01.CSharp概述.Listing01_16.Listing01_16.Main);
+        }
+    }
+}
---
+++ Src/Chapter01.CSharp概述/Listing01_16.使用字符串插值_美元符_进行格式化.cs
@@ -0,0 +1,34 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 11:00:07
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 11:00:07
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter01.CSharp概述.Listing01_16
+{
+    public class Listing01_16
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hey You!");
+            Console.WriteLine("Please enter your first name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please enter your last name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine($"Your full name is {firstName} {lastName}.");
+        }
+    }
+}
