ddd545b2df06b06c65813958f28224d06dc444f0
Fowindy <47002339@qq.com>
2020-07-22 09:59:12 +0800

    #Listing01_15.从控制台获取输入_Read+测试

---
+++ Src/Chapter01.CSharp概述.Test/Listing01_15.从控制台获取输入_Read.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 09:50:24
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 09:50:24
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter01.CSharp概述.Test.Listing01_15
+{
+    [TestClass]
+    public class Listing01_15
+    {
+        [TestMethod]
+        public void Main_Listing01_15()
+        {
+            const string expected =
+                @"s<<s>>";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter01.CSharp概述.Listing01_15.Listing01_15.Main);
+        }
+    }
+}
---
+++ Src/Chapter01.CSharp概述/Listing01_15.从控制台获取输入_Read.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 09:43:55
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 09:43:55
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter01.CSharp概述.Listing01_15
+{
+    public class Listing01_15
+    {
+        public static void Main()
+        {
+            int readValue;
+            char character;
+            readValue = Console.Read();
+            character = (char)readValue;
+            Console.Write(character);
+        }
+    }
+}
