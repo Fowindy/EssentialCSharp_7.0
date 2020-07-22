bc7b7f0dda7f501d3c2848156d380bae6eab5bad
Fowindy <47002339@qq.com>
2020-07-22 16:16:23 +0800

    #Listing02_01.指定字面值.Test

---
+++ Src/Chapter02.数据类型.Test/Listing02_01.指定字面值.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 16:10:01
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 16:10:01
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter02.数据类型.Test.Listing02_01
+{
+    [TestClass]
+    public class Listing02_01
+    {
+        [TestMethod]
+        public void Main_Listing02_01()
+        {
+            const string expected =
+                @"42
+1.618034";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter02.数据类型.Listing02_01.Listing02_01.Main);
+        }
+    }
+}
