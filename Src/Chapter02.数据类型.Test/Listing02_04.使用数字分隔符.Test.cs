3de1cf30004f869cd0f117dd144de8467609f27e
Fowindy <47002339@qq.com>
2020-07-22 19:42:02 +0800

    #Listing02_04.使用数字分隔符+测试

---
+++ Src/Chapter02.数据类型.Test/Listing02_04.使用数字分隔符.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 18:19:46
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 18:19:46
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter02.数据类型.Test.Listing02_04
+{
+    [TestClass]
+    public class Listing02_04
+    {
+        [TestMethod]
+        public void Main_Listing02_04()
+        {
+            const string expected =
+                @"9814072356";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter02.数据类型.Listing02_04.Listing02_04.Main);
+        }
+    }
+}
