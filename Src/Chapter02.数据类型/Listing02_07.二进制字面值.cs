12826225d3f1a408a923134df7405bda29155bd3
Fowindy <47002339@qq.com>
2020-07-22 22:40:22 +0800

    #Listing02_07.二进制字面值+测试

---
+++ Src/Chapter02.数据类型/Listing02_07.二进制字面值.cs
@@ -0,0 +1,30 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 22:32:24
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 22:32:24
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter02.数据类型.Listing02_07
+{
+    public class Listing02_07
+    {
+        public static void Main()
+        {
+            //用二进制比表示42
+            Console.WriteLine(0b101010);
+        }
+    }
+}
