33576403cad60e21ce3732d1546c553b0e676e0a
Fowindy <47002339@qq.com>
2020-07-22 09:00:34 +0800

    #Listing01_13.赋值会返回值_该值可以再次用于赋值+测试

---
+++ Src/Chapter01.CSharp概述.Test/Listing01_13.赋值会返回值_该值可以再次用于赋值.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 08:59:31
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 08:59:31
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter01.CSharp概述.Test.Listing01_13
+{
+    [TestClass]
+    public class Listing01_13
+    {
+        [TestMethod]
+        public void Main_Listing01_13()
+        {
+            const string expected =
+                @"It would take a miracle.
+It would take a miracle.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter01.CSharp概述.Listing01_13.Listing01_13.Main);
+        }
+    }
+}
---
+++ Src/Chapter01.CSharp概述/Listing01_13.赋值会返回值_该值可以再次用于赋值.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月22日 星期三 08:55:24
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月22日 星期三 08:55:24
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter01.CSharp概述.Listing01_13
+{
+    public class Listing01_13
+    {
+        public static void Main()
+        {
+            string requirements, miracleMax;
+            requirements = miracleMax = "It would take a miracle.";
+            Console.WriteLine(requirements);
+            Console.WriteLine(miracleMax);
+        }
+    }
+}
