1ce0ffcda6143a435da15bf20bca6b82095b45e7
Fowindy <47002339@qq.com>
2020-07-23 11:28:15 +0800

    #Listing02_12.使用Unicode编码显示笑脸符号+测试+Unicode笔记

---
+++ Src/Chapter02.数据类型/Listing02_12.使用Unicode编码显示笑脸符号.cs
@@ -0,0 +1,30 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月23日 星期四 11:21:11
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月23日 星期四 11:21:11
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter02.数据类型.Listing02_12
+{
+    public class Listing02_12
+    {
+        public static void Main()
+        {
+            Console.Write('\u003A');
+            Console.WriteLine("\u0029");
+        }
+    }
+}
