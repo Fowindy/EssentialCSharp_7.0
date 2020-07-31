a59c23abd09b3ec35cc1b3ae6b44feeae9ee5935
Fowindy <47002339@qq.com>
2020-07-31 13:39:25 +0800

    #Listing02_14.使用@逐字字符串字面值显示三角形+测试

---
+++ Src/Chapter02.数据类型.Test/Listing02_14.使用@逐字字符串字面值显示三角形.Test.cs
@@ -0,0 +1,38 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年7月31日 星期五 13:33:39
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年7月31日 星期五 13:33:39
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter02.数据类型.Test.Listing02_14
+{
+    [TestClass]
+    public class Listing02_14
+    {
+        [TestMethod]
+        public void Main_Listing02_14()
+        {
+            const string expected =@"begin
+                /\
+               /  \
+              /    \
+             /      \
+            /        \
+           /          \
+          /____________\
+end";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter02.数据类型.Listing02_14.Listing02_14.Main);
+        }
+    }
+}
