4661631a2e9035791a893acd99e8aeb9270590e1
Fowindy <47002339@qq.com>
2020-08-12 08:07:35 +0800

    #Listing02_16.string的实例方法

---
+++ Src/Chapter02.数据类型/Listing02_16.string的实例方法.cs
@@ -0,0 +1,28 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月12日 星期三 07:57:12
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月12日 星期三 07:57:12
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter02.数据类型.Listing02_16
+{
+    public class Listing02_16
+    {
+        public static void Main()
+        {
+        }
+    }
+}
