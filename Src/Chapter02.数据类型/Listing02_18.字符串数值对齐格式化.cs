5500eec61f4919ac8156b7886a91d89200852110
Fowindy <47002339@qq.com>
2020-08-15 07:30:49 +0800

    #Listing02_18.字符串数值对齐格式化

---
+++ Src/Chapter02.数据类型/Listing02_18.字符串数值对齐格式化.cs
@@ -0,0 +1,28 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月15日 星期六 07:27:12
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月15日 星期六 07:27:12
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter02.数据类型.Listing02_18
+{
+    public class Listing02_18
+    {
+        public static void Main()
+        {
+        }
+    }
+}
