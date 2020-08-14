9d8ba5674559f1080591eb419cc64a89485868f9
Fowindy <47002339@qq.com>
2020-08-14 23:29:04 +0800

    #Listing02_17.using static指令的引用_原理讲解

---
+++ Src/Chapter02.数据类型/Listing02_17.using static指令的引用.cs
@@ -0,0 +1,28 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月14日 星期五 23:25:57
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月14日 星期五 23:25:57
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter02.数据类型.Listing02_17
+{
+    public class Listing02_17
+    {
+        public static void Main()
+        {
+        }
+    }
+}
