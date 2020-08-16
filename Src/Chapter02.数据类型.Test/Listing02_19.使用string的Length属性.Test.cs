0846855d5986f20beee2aa664f1ca1a643d349d6
Fowindy <47002339@qq.com>
2020-08-16 08:05:53 +0800

    #Listing02_19.使用string的Length属性+测试

---
+++ Src/Chapter02.数据类型.Test/Listing02_19.使用string的Length属性.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月16日 星期日 07:55:09
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月16日 星期日 07:55:09
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter02.数据类型.Test.Listing02_19
+{
+    [TestClass]
+    public class Listing02_19
+    {
+        [TestMethod]
+        public void Main_Listing02_19()
+        {
+            const string expected =
+                @"Please enter a palindrome(回文):<<asdfghhgfdsa
+>>The palindrome ""asdfghhgfdsa"" is 12 characters.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter02.数据类型.Listing02_19.Listing02_19.Main);
+        }
+    }
+}
---
+++ Src/Chapter02.数据类型/Listing02_19.使用string的Length属性.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月16日 星期日 07:47:55
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月16日 星期日 07:47:55
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter02.数据类型.Listing02_19
+{
+    public class Listing02_19
+    {
+        public static void Main()
+        {
+            string palindrome;
+            Console.Write("Please enter a palindrome(回文):");
+            palindrome = Console.ReadLine();
+            Console.WriteLine($"The palindrome \"{palindrome}\" is" +
+                $" {palindrome.Length} characters.");
+        }
+    }
+}
