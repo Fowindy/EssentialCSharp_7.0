0a9a95b88d404deacf8ed70570d8206c8520353c
Fowindy <47002339@qq.com>
2020-08-16 09:28:41 +0800

    #Listing02_22.将null赋值给字符串变量+测试+重要_修复2.18测试空格和2.22测试换行的问题

--- Src/Chapter02.数据类型.Test/Listing02_22.将null赋值给字符串变量.Test.cs
+++ Src/Chapter02.数据类型.Test/Listing02_22.将null赋值给字符串变量.Test.cs
@@ -3,31 +3,32 @@
  * 
  * 作者(Author)：Fowindy
  * 
- * 日期(Create Date)：2020年8月15日 星期六 07:31:47
+ * 日期(Create Date)：2020年8月16日 星期日 08:31:06
  * 
  * 修改记录(Revision History)：
  *      R1：
  *          修改作者：Fowindy
- *          修改日期：2020年8月15日 星期六 07:31:47
+ *          修改日期：2020年8月16日 星期日 08:31:06
  *          修改理由：创建文档
  * ==============================================================================*/
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Fowindy.Chapter02.数据类型.Test.Listing02_18
+namespace Fowindy.Chapter02.数据类型.Test.Listing02_22
 {
     [TestClass]
-    public class Listing02_18
+    public class Listing02_22
     {
         [TestMethod]
-        public void Main_Listing02_18()
+        public void Main_Listing02_22()
         {
             const string expected =
-                @"Please enter a decimal value:<<123456789012.345678
->> ¥123,456,789,012.35
->> ¥123,456,789,012.35";
+                @"Please enter faxNumber:<<123234
+>>123234
+
+";
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
-                expected, Chapter02.数据类型.Listing02_18.Listing02_18.Main);
+                expected, Chapter02.数据类型.Listing02_22.Listing02_22.Main);
         }
     }
 }
---
+++ Src/Chapter02.数据类型/Listing02_22.将null赋值给字符串变量.cs
@@ -0,0 +1,34 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月16日 星期日 08:28:23
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月16日 星期日 08:28:23
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter02.数据类型.Listing02_22
+{
+    public class Listing02_22
+    {
+        public static void Main()
+        {
+            string faxNumber;
+            Console.Write("Please enter faxNumber:");
+            faxNumber = Console.ReadLine();
+            Console.WriteLine(faxNumber);
+            faxNumber = null;
+            Console.WriteLine(faxNumber);
+        }
+    }
+}
