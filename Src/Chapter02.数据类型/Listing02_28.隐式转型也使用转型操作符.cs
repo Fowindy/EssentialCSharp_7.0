b144bf9be213668a9c3bbf7d4b9df27ce382556a
Fowindy <47002339@qq.com>
2020-08-17 19:01:21 +0800

    Listing02_28.隐式转型也使用转型操作符+测试

---
+++ Src/Chapter02.数据类型/Listing02_28.隐式转型也使用转型操作符.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月17日 星期一 18:58:09
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月17日 星期一 18:58:09
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter02.数据类型.Listing02_28
+{
+    public class Listing02_28
+    {
+        public static void Main()
+        {
+            int intNumber = 31416;
+            long longNumber = (long)intNumber;
+            Console.WriteLine(longNumber);
+        }
+    }
+}
