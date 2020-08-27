491733906de3fbc9cbcb5f97e1ea457acf94217c
Fowindy <47002339@qq.com>
2020-08-27 11:09:03 +0800

    Listing02_32.用TryParse()代替引发异常+测试

--- Src/Chapter02.数据类型.Test/Listing02_32.用TryParse()代替引发异常.Test.cs
+++ Src/Chapter02.数据类型.Test/Listing02_32.用TryParse()代替引发异常.Test.cs
@@ -3,29 +3,30 @@
  * 
  * 作者(Author)：Fowindy
  * 
- * 日期(Create Date)：2020年8月27日 星期四 01:26:44
+ * 日期(Create Date)：2020年8月27日 星期四 11:05:31
  * 
  * 修改记录(Revision History)：
  *      R1：
  *          修改作者：Fowindy
- *          修改日期：2020年8月27日 星期四 01:26:44
+ *          修改日期：2020年8月27日 星期四 11:05:31
  *          修改理由：创建文档
  * ==============================================================================*/
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Fowindy.Chapter02.数据类型.Test.Listing02_31
+namespace Fowindy.Chapter02.数据类型.Test.Listing02_32
 {
     [TestClass]
-    public class Listing02_31
+    public class Listing02_32
     {
         [TestMethod]
-        public void Main_Listing02_31()
+        public void Main_Listing02_32()
         {
             const string expected =
-                @"True";
+                @"Please enter a number:<<42
+>>Converted correctly,the number is:42";
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
-                expected, Chapter02.数据类型.Listing02_31.Listing02_31.Main);
+                expected, Chapter02.数据类型.Listing02_32.Listing02_32.Main);
         }
     }
 }
