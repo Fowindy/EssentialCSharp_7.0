a9c4645b9cbc9d62b2bbd6cb110f81efadaa8345
Fowindy <47002339@qq.com>
2020-08-28 12:18:30 +0800

    Listing03_03_10.元组_通过变量和属性名腿短元组项名称

---
+++ Src/Chapter03.更多数据类型/Listing03_03_10.元组_通过变量和属性名腿短元组项名称.cs
@@ -0,0 +1,35 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月28日 星期五 12:13:53
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月28日 星期五 12:13:53
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter03.更多数据类型.Listing03_03_10
+{
+    public class Listing03_03_10
+    {
+        public static void Main()
+        {
+            string country = "Malawi";
+            string capital = "Lilongwe";
+            double gdpPerCapital = 226.50;
+            var countryInfo = (country, capital, gdpPerCapital);
+            //C# 7.1版本
+//            Console.WriteLine($@"The poorest country in the world in 2017 was {
+//countryInfo.country},{countryInfo.capital}:{countryInfo.gdpPerCapital}");
+        }
+    }
+}
