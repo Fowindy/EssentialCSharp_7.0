bced9ee0c68adc431b385b33b18bce1e82806d23
Fowindy <47002339@qq.com>
2020-08-30 22:08:08 +0800

    Listing03_15.大小不一致的多维数组会造成错误

---
+++ Src/Chapter03.更多数据类型/Listing03_15.大小不一致的多维数组会造成错误.cs
@@ -0,0 +1,35 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月30日 星期日 22:05:57
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月30日 星期日 22:05:57
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter03.更多数据类型.Listing03_15
+{
+    public class Listing03_15
+    {
+        public static void Main()
+        {
+            //错误:每一维元素的大小必须完全一样
+            //int[,] cells = {
+            //    { 1,0,2,0},
+            //    { 1,2,0},
+            //    { 1,2},
+            //    { 1}
+            //};
+        }
+    }
+}
