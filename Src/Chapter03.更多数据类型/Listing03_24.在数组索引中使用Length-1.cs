5b3959a95c9282e4f8b55e72619767f034e4a0f9
Fowindy <47002339@qq.com>
2020-08-31 00:28:25 +0800

    Listing03_24.在数组索引中使用Length-1

---
+++ Src/Chapter03.更多数据类型/Listing03_24.在数组索引中使用Length-1.cs
@@ -0,0 +1,30 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月31日 星期一 00:27:04
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月31日 星期一 00:27:04
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter03.更多数据类型.Listing03_24
+{
+    public class Listing03_24
+    {
+        public static void Main()
+        {
+            string[] languages = new string[9];
+            languages[4] = languages[languages.Length - 1];
+        }
+    }
+}
