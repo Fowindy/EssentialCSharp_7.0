2700dfcb37f1d8f969ebf0e1ffef6a77ac1073fa
Fowindy <47002339@qq.com>
2020-08-31 08:50:18 +0800

    数组实例方法

---
+++ Src/Chapter03.更多数据类型.Test/Listing03_26.获取特定维的大小.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月31日 星期一 08:52:54
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月31日 星期一 08:52:54
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter03.更多数据类型.Test.Listing03_26
+{
+    [TestClass]
+    public class Listing03_26
+    {
+        [TestMethod]
+        public void Main_Listing03_26()
+        {
+            const string expected =
+                @"2
+3";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter03.更多数据类型.Listing03_26.Listing03_26.Main);
+        }
+    }
+}
---
+++ Src/Chapter03.更多数据类型/Listing03_26.获取特定维的大小.cs
@@ -0,0 +1,34 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月31日 星期一 08:50:49
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月31日 星期一 08:50:49
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter03.更多数据类型.Listing03_26
+{
+    public class Listing03_26
+    {
+        public static void Main()
+        {
+            bool[,,] cells;
+            cells = new bool[2, 3, 3];
+            //获取一维元素的长度
+            Console.WriteLine(cells.GetLength(0));
+            //获取整个数组的维数
+            Console.WriteLine(cells.Rank);
+        }
+    }
+}
