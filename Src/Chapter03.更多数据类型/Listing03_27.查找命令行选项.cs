8f1d6bc4a5d034100781b55ea7f5bb9b63696c62
Fowindy <47002339@qq.com>
2020-08-31 09:01:06 +0800

    Listing03_27.查找命令行选项

---
+++ Src/Chapter03.更多数据类型/Listing03_27.查找命令行选项.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年8月31日 星期一 08:57:35
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年8月31日 星期一 08:57:35
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter03.更多数据类型.Listing03_27
+{
+    public class Listing03_27
+    {
+        public static void Main()
+        {
+            string[] args = new string[6];
+            if (args[0][0] == '-')
+            {
+                //这个参数是一个操作符
+            }
+        }
+    }
+}
