63d54417c9b16371043e6c225b89af0926bed47e
Fowindy <470026339@qq.com>
2020-09-01 12:34:27 +0800

    Listing04_01.简单操作符例子

---
+++ Src/Chapter04.操作符和控制流程/Listing04_01.简单操作符例子.cs
@@ -0,0 +1,29 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月1日 星期二 12:33:31
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月1日 星期二 12:33:31
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_01
+{
+    public class Listing04_01
+    {
+        public static void Main()
+        {
+            int difference = 4 - 2;
+        }
+    }
+}
