4984258ff58cd685c966a1fa390f42d14c68e9c5
Fowindy <470026339@qq.com>
2020-09-01 12:40:18 +0800

    Listing04_02.指定负值

---
+++ Src/Chapter04.操作符和控制流程/Listing04_02.指定负值.cs
@@ -0,0 +1,30 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月1日 星期二 12:38:35
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月1日 星期二 12:38:35
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_02
+{
+    public class Listing04_02
+    {
+        public static void Main()
+        {
+            //National debt to be penny
+            decimal debt = -2020366853807.76M;
+        }
+    }
+}
