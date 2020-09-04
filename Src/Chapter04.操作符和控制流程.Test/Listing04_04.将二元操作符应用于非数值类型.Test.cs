47f537aa9fd78a30be415fa19d280b5115ae4a31
Fowindy <47002339@qq.com>
2020-09-04 23:00:24 +0800

    Listing04_04.将二元操作符应用于非数值类型

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_04.将二元操作符应用于非数值类型.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月4日 星期五 22:59:14
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月4日 星期五 22:59:14
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_04
+{
+    [TestClass]
+    public class Listing04_04
+    {
+        [TestMethod]
+        public void Main_Listing04_04()
+        {
+            const string expected =
+                @"The original Tacoma Bridge in Washington
+was brought down by a windSpeed mile/hour wind.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_04.Listing04_04.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_04.将二元操作符应用于非数值类型.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月4日 星期五 22:51:57
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月4日 星期五 22:51:57
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_04
+{
+    public class Listing04_04
+    {
+        public static void Main()
+        {
+            short windSpeed = 42;
+            Console.WriteLine(
+                "The original Tacoma Bridge in Washington\nwas" +
+                " brought down by a" +
+                " windSpeed" + " mile/hour wind.");
+        }
+    }
+}
