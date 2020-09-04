f6b63255929e2c5f388974ff104d624f63dd4a8b
Fowindy <47002339@qq.com>
2020-09-05 07:58:56 +0800

    Listing04_06.判断两个字符之间的距离+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_05.将加法操作符应用于char数据类型.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月4日 星期五 23:52:13
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月4日 星期五 23:52:13
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_05
+{
+    [TestClass]
+    public class Listing04_05
+    {
+        [TestMethod]
+        public void Main_Listing04_05()
+        {
+            const string expected =
+                @"g";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_05.Listing04_05.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_05.将加法操作符应用于char数据类型.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月4日 星期五 23:03:44
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月4日 星期五 23:03:44
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_05
+{
+    public class Listing04_05
+    {
+        public static void Main()
+        {
+            //相加的是字符的Unicode值
+            int n = '3' + '4';
+            char c = (char)n;
+            Console.WriteLine(c);
+        }
+    }
+}
