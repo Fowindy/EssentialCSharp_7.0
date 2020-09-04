e95a23923d6625219815fc94b55eb4b4de64c58e
Fowindy <470026339@qq.com>
2020-09-04 22:27:41 +0800

    Listing04_03.使用二元操作符+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_03.使用二元操作符.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月1日 星期二 13:06:29
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月1日 星期二 13:06:29
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_03
+{
+    [TestClass]
+    public class Listing04_03
+    {
+        [TestMethod]
+        public void Main_Listing04_03()
+        {
+            const string expected =
+                @"Please Enter the numerator:<<23
+>>Please Enter the denominator:<<4
+>>23 / 4 = 5 with remainder 3";
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_03.Listing04_03.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_03.使用二元操作符.cs
@@ -0,0 +1,43 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月1日 星期二 12:55:39
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月1日 星期二 12:55:39
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_03
+{
+    public class Listing04_03
+    {
+        public static void Main()
+        {
+            int numerator;
+            int denominator;
+            int quotient;
+            int remainder;
+
+            Console.Write("Please Enter the numerator:");
+            numerator = int.Parse(Console.ReadLine());
+
+            Console.Write("Please Enter the denominator:");
+            denominator = int.Parse(Console.ReadLine());
+
+            quotient = numerator / denominator;
+            remainder = numerator % denominator;
+
+            Console.WriteLine($"{numerator} / {denominator} = {quotient} with remainder {remainder}");
+        }
+    }
+}
