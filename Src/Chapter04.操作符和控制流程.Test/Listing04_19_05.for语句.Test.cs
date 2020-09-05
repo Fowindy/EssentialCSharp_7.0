3cb30d59c120d07b1dc5dda4918c15565a5229b7
Fowindy <47002339@qq.com>
2020-09-05 17:29:46 +0800

    Listing04_19_05.for语句+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_19_05.for语句.Test.cs
@@ -0,0 +1,40 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 17:28:39
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 17:28:39
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_19_05
+{
+    [TestClass]
+    public class Listing04_19_05
+    {
+        [TestMethod]
+        public void Main_Listing04_19_05()
+        {
+            const string expected =
+                @"count = 1
+count = 2
+count = 3
+count = 4
+count = 5
+count = 6
+count = 7
+count = 8
+count = 9
+count = 10";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_19_05.Listing04_19_05.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_19_05.for语句.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 17:25:03
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 17:25:03
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_19_05
+{
+    public class Listing04_19_05
+    {
+        public static void Main()
+        {
+            for (int count = 1;count <= 10;count++)
+            {
+                Console.WriteLine($"count = {count}");
+            }
+        }
+    }
+}
