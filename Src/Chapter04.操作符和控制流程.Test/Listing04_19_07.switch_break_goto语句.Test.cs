4c9fb49cf0f2047720f3f45a2b390700fe92804e
Fowindy <47002339@qq.com>
2020-09-05 17:46:41 +0800

    Listing04_19_07.switch_break_goto语句+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_19_07.switch_break_goto语句.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 17:45:16
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 17:45:16
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_19_07
+{
+    [TestClass]
+    public class Listing04_19_07
+    {
+        [TestMethod]
+        public void Main_Listing04_19_07()
+        {
+            const string expected =
+                @"Please enter command:<<exit
+>>Exiting app...";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_19_07.Listing04_19_07.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_19_07.switch_break_goto语句.cs
@@ -0,0 +1,56 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 17:40:20
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 17:40:20
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_19_07
+{
+    public class Listing04_19_07
+    {
+        public static void Main()
+        {
+            Console.Write("Please enter command:");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "exit":
+                case "quit":
+                    Console.WriteLine("Exiting app...");
+                    break;
+                case "restart":
+                    Reset();
+                    goto case "start";
+                case "start":
+                    GetMove();
+                    break;
+                default:
+                    Console.WriteLine(input);
+                    break;
+            }
+        }
+
+        private static void GetMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
