1496f361fc183c552345076823853006dda1e8e7
Fowindy <47002339@qq.com>
2020-09-05 10:32:09 +0800

    Listing04_08.浮点数被零除的结果是NaN+测试

---
+++ Src/Chapter04.操作符和控制流程/Listing04_08.浮点数被零除的结果是NaN.cs
@@ -0,0 +1,30 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 10:28:38
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 10:28:38
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_08
+{
+    public class Listing04_08
+    {
+        public static void Main()
+        {
+            float n = 0f;
+            Console.WriteLine(n/0);
+        }
+    }
+}
