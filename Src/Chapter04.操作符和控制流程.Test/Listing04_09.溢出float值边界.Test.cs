216d43c365f7320f72f32409ba98dd25ceeb33b9
Fowindy <47002339@qq.com>
2020-09-05 10:39:43 +0800

    Listing04_09.溢出float值边界+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_09.溢出float值边界.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 10:37:46
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 10:37:46
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_09
+{
+    [TestClass]
+    public class Listing04_09
+    {
+        [TestMethod]
+        public void Main_Listing04_09()
+        {
+            const string expected =
+                @"-∞
+∞";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_09.Listing04_09.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_09.溢出float值边界.cs
@@ -0,0 +1,30 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 10:36:16
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 10:36:16
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_09
+{
+    public class Listing04_09
+    {
+        public static void Main()
+        {
+            Console.WriteLine(-1f/0);
+            Console.WriteLine(1f/0);
+        }
+    }
+}
