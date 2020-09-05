bd5720b8cf13c540404db7c940bc8788db7407fd
Fowindy <47002339@qq.com>
2020-09-06 06:51:09 +0800

    Listing04_20.if_else语句示例+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_20.if_else语句示例.Test.cs
@@ -0,0 +1,34 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 06:48:51
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 06:48:51
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_20
+{
+    [TestClass]
+    public class Listing04_20
+    {
+        [TestMethod]
+        public void Main_Listing04_20()
+        {
+            const string expected =
+                @"1---Play against the computer
+2---Play against another player.
+Choose:<<1
+>>Play against computer selected.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_20.Listing04_20.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_20.if_else语句示例.cs
@@ -0,0 +1,37 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 06:41:17
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 06:41:17
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_20
+{
+    public class Listing04_20
+    {
+        public static void Main()
+        {
+            string input;
+            Console.Write("1---Play against the computer\n"+
+                "2---Play against another player.\n"+
+                "Choose:");
+            input = Console.ReadLine();
+            if (input == "1")
+                Console.WriteLine("Play against computer selected.");
+            else
+                Console.WriteLine("Play against another player.");
+        }
+    }
+}
