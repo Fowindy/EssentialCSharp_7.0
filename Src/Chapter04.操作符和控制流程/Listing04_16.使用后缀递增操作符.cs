bb2d6fae2e9521ba2fbf222ee2c100abf53df23a
Fowindy <47002339@qq.com>
2020-09-05 12:25:55 +0800

    Listing04_16.使用后缀递增操作符+测试

---
+++ Src/Chapter04.操作符和控制流程/Listing04_16.使用后缀递增操作符.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 11:59:38
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 11:59:38
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_16
+{
+    public class Listing04_16
+    {
+        public static void Main()
+        {
+            int count = 123;
+            int result;
+            result = count++;
+            Console.WriteLine($"result = {result} and count = {count}");
+        }
+    }
+}
