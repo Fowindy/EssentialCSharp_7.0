0e8a709d7ee3f7fd9beec71d8f76743a9cf45721
Fowindy <47002339@qq.com>
2020-09-05 11:41:37 +0800

    Listing04_13.递增操作符+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_13.递增操作符.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 11:40:32
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 11:40:32
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_13
+{
+    [TestClass]
+    public class Listing04_13
+    {
+        [TestMethod]
+        public void Main_Listing04_13()
+        {
+            const string expected =
+                @"126";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_13.Listing04_13.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_13.递增操作符.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 11:38:51
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 11:38:51
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_13
+{
+    public class Listing04_13
+    {
+        public static void Main()
+        {
+            int spaceCount = 123;
+            spaceCount = spaceCount + 1;
+            spaceCount += 1;
+            spaceCount++;
+            Console.WriteLine(spaceCount);
+        }
+    }
+}
