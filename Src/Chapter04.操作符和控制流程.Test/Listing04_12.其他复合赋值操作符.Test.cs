0cbb439ff64fd8f45d2415a4f3d9ddcd14316501
Fowindy <47002339@qq.com>
2020-09-05 11:36:46 +0800

    Listing04_12.其他复合赋值操作符+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_12.其他复合赋值操作符.Test.cs
@@ -0,0 +1,34 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 11:35:40
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 11:35:40
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_12
+{
+    [TestClass]
+    public class Listing04_12
+    {
+        [TestMethod]
+        public void Main_Listing04_12()
+        {
+            const string expected =
+                @"123 / 2 = 61
+61 * 2 = 122
+122 - 2 = 120
+120 % 2 = 0";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_12.Listing04_12.Main);
+        }
+    }
+}
