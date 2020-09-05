68c56636461ead596de645c8577bd576cc26109e
Fowindy <47002339@qq.com>
2020-09-05 11:57:26 +0800

    Listing04_15.降序显示每个字母的ASCI1值+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_15.降序显示每个字母的ASCI1值.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 11:51:04
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 11:51:04
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_15
+{
+    [TestClass]
+    public class Listing04_15
+    {
+        [TestMethod]
+        public void Main_Listing04_15()
+        {
+            const string expected =
+                "z=122\ty=121\tx=120\tw=119\tv=118\tu=117\tt=116\ts=115\tr=114\tq=113\tp=112\to=111\tn=110\tm=109\tl=108\tk=107\tj=106\ti=105\th=104\tg=103\tf=102\te=101\td=100\tc=99\tb=98\ta=97\t";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_15.Listing04_15.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_15.降序显示每个字母的ASCI1值.cs
@@ -0,0 +1,39 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 11:45:10
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 11:45:10
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_15
+{
+    public class Listing04_15
+    {
+        public static void Main()
+        {
+            char current;
+            int unicodeValue;
+            current = 'z';
+            do 
+            {
+                //把当前的字符转换为int类型的unicode
+                unicodeValue = current;
+                Console.Write($"{current}={unicodeValue}\t");
+                //到字母表中的前一个字符
+                current--;
+            } while (current >= 'a');
+        }
+    }
+}
