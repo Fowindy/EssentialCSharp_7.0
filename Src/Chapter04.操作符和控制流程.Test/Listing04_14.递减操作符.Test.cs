7a939c5d5405d8452ca4f4f0f777e498214a62f6
Fowindy <47002339@qq.com>
2020-09-05 11:44:50 +0800

    初学者主题:循环中的递减示例

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_14.递减操作符.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 11:43:20
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 11:43:20
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_14
+{
+    [TestClass]
+    public class Listing04_14
+    {
+        [TestMethod]
+        public void Main_Listing04_14()
+        {
+            const string expected =
+                @"120";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_14.Listing04_14.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_14.递减操作符.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 11:42:17
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 11:42:17
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_14
+{
+    public class Listing04_14
+    {
+        public static void Main()
+        {
+            int lines = 123;
+            lines = lines - 1;
+            lines -= 1;
+            lines--;
+            Console.WriteLine(lines);
+        }
+    }
+}
