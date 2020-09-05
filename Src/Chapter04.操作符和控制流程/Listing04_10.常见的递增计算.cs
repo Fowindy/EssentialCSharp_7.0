031bf290cd7773fdbdb551c4028ef47848d8c182
Fowindy <47002339@qq.com>
2020-09-05 10:43:21 +0800

    Listing04_10.常见的递增计算+测试

---
+++ Src/Chapter04.操作符和控制流程/Listing04_10.常见的递增计算.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 10:41:28
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 10:41:28
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_10
+{
+    public class Listing04_10
+    {
+        public static void Main()
+        {
+            int x = 123;
+            x = x + 2;
+            Console.WriteLine(x);
+        }
+    }
+}
