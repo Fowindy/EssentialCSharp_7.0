deb8a4a2193fa7b39dd9ba33bcedc803eb5b3478
Fowindy <47002339@qq.com>
2020-09-05 10:45:26 +0800

    Listing04_11.使用+=操作符+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_11.使用+=操作符.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 10:44:51
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 10:44:51
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_11
+{
+    [TestClass]
+    public class Listing04_11
+    {
+        [TestMethod]
+        public void Main_Listing04_11()
+        {
+            const string expected =
+                @"125";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_11.Listing04_11.Main);
+        }
+    }
+}
