ff800bcba8527737485a5159825f03a314eb1f54
Fowindy <47002339@qq.com>
2020-09-05 17:38:38 +0800

    Listing04_19_06.foreach语句+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_19_06.foreach语句.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 17:37:29
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 17:37:29
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_19_06
+{
+    [TestClass]
+    public class Listing04_19_06
+    {
+        [TestMethod]
+        public void Main_Listing04_19_06()
+        {
+            const string expected =
+                @"Please enter your email address:<<470026339@qq.com
+>>qq.com";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_19_06.Listing04_19_06.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_19_06.foreach语句.cs
@@ -0,0 +1,43 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月5日 星期六 17:30:10
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月5日 星期六 17:30:10
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_19_06
+{
+    public class Listing04_19_06
+    {
+        public static void Main()
+        {
+            Console.Write("Please enter your email address:");
+            string email = Console.ReadLine();
+            bool insideDomain = false;
+            foreach (char letter in email)
+            {
+                if (!insideDomain)
+                {
+                    if (letter == '@')
+                    {
+                        insideDomain = true;
+                    }
+                    continue;
+                }
+                Console.Write(letter);
+            }
+        }
+    }
+}
