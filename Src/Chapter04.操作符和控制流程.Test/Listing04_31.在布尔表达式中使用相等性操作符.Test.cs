22ae70ceb3d4fb32364ca169952fb3cac661de15
Fowindy <47002339@qq.com>
2020-09-06 13:32:44 +0800

    Listing04_31.在布尔表达式中使用相等性操作符+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_31.在布尔表达式中使用相等性操作符.Test.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 13:31:30
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 13:31:30
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_31
+{
+    [TestClass]
+    public class Listing04_31
+    {
+        [TestMethod]
+        public void Main_Listing04_31()
+        {
+            const string expected =
+                @"Please enter player number:<<1
+>>Please enter command:<<quit
+>>Player 1 quit!!";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_31.Listing04_31.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_31.在布尔表达式中使用相等性操作符.cs
@@ -0,0 +1,37 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 13:26:48
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 13:26:48
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_31
+{
+    public class Listing04_31
+    {
+        public static void Main()
+        {
+            Console.Write("Please enter player number:");
+            int playerNumber = int.Parse(Console.ReadLine());
+            Console.Write("Please enter command:");
+            string input = Console.ReadLine();
+            if (input == "quit" || input == "exit")
+            {
+                Console.WriteLine($"Player {playerNumber} quit!!");
+                //break;
+            }
+        }
+    }
+}
