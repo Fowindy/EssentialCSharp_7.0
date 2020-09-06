cf96ba606e706c351716f0e5bab37f96ea551858
Fowindy <47002339@qq.com>
2020-09-06 16:33:41 +0800

    Listing04_32.使用OR操作符+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_32.使用OR操作符.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 16:36:40
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 16:36:40
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_32
+{
+    [TestClass]
+    public class Listing04_32
+    {
+        [TestMethod]
+        public void Main_Listing04_32()
+        {
+            const string expected =
+                @"请输入现在是几点:<<25
+>>The time you entered is invalid.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_32.Listing04_32.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_32.使用OR操作符.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 13:45:27
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 13:45:27
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_32
+{
+    public class Listing04_32
+    {
+        public static void Main()
+        {
+            Console.Write("请输入现在是几点:");
+            int hourofTheDay = int.Parse(Console.ReadLine());
+            if ((hourofTheDay > 23) || (hourofTheDay < 0))
+            Console.WriteLine("The time you entered is invalid.");
+        }
+    }
+}
