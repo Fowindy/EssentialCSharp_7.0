3eeed00b5075fe680795d966d46c9a65bdd8115f
Fowindy <47002339@qq.com>
2020-09-06 21:26:45 +0800

    Listing04_39.使用右移位操作符_相当于除以+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_39.使用右移位操作符_相当于除以.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 21:25:27
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 21:25:27
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_39
+{
+    [TestClass]
+    public class Listing04_39
+    {
+        [TestMethod]
+        public void Main_Listing04_39()
+        {
+            const string expected =
+                @"x = -2";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_39.Listing04_39.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_39.使用右移位操作符_相当于除以.cs
@@ -0,0 +1,36 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 21:19:36
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 21:19:36
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_39
+{
+    public class Listing04_39
+    {
+        public static void Main()
+        {
+            int x;
+            x = (-7 >> 2);//11111001
+                          //11111110
+                          //-1-1 = -2
+                          //右移相当于除2的2次方
+                          //-7/4 = -2
+            Console.WriteLine($"x = {x}");
+
+        }
+    }
+}
