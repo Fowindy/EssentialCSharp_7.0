c3660316d90c9278fb020886e96a46fb3546b856
Fowindy <47002339@qq.com>
2020-09-06 13:26:13 +0800

    Listing04_30.将关系操作符的结果赋给bool变量+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_30.将关系操作符的结果赋给bool变量.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 13:25:32
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 13:25:32
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_30
+{
+    [TestClass]
+    public class Listing04_30
+    {
+        [TestMethod]
+        public void Main_Listing04_30()
+        {
+            const string expected =
+                @"True";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_30.Listing04_30.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_30.将关系操作符的结果赋给bool变量.cs
@@ -0,0 +1,30 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 13:24:29
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 13:24:29
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_30
+{
+    public class Listing04_30
+    {
+        public static void Main()
+        {
+            bool result = 70 > 7;
+            Console.WriteLine(result);
+        }
+    }
+}
