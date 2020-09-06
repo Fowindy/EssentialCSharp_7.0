275e840fa6d024ba0b30f22d1fa67335901255a6
Fowindy <47002339@qq.com>
2020-09-06 16:52:51 +0800

    Listing04_34.使用逻辑求反操作符+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_34.使用逻辑求反操作符.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 16:50:14
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 16:50:14
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_34
+{
+    [TestClass]
+    public class Listing04_34
+    {
+        [TestMethod]
+        public void Main_Listing04_34()
+        {
+            const string expected =
+                @"result = True";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_34.Listing04_34.Main);
+        }
+    }
+}
