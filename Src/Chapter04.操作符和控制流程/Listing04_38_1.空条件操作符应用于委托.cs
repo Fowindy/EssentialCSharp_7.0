03e3bc984c1ad5ae14e6d16d9c364ef657579d59
Fowindy <47002339@qq.com>
2020-09-06 20:02:44 +0800

    Listing04_38_1.空条件操作符应用于委托

---
+++ Src/Chapter04.操作符和控制流程/Listing04_38_1.空条件操作符应用于委托.cs
@@ -0,0 +1,36 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 20:01:03
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 20:01:03
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_38_1
+{
+    public class Listing04_38_1
+    {
+        public static void Main()
+        {
+            //            PropertyChangedEventHandler propertyChanged =
+            //PropertyChanged;
+            //            if (propertychanged != null)
+            //                propertyChanged(this,
+            //                new PropertyChangedEventArgs(nameof(Name)));
+
+//            PropertyChanged?.Invoke(propertyChanged(
+//this, new PropertyChangedEventArgs(nameof(Name)));
+        }
+    }
+}
