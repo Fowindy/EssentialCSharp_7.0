c0e1eda23204a14541aa6dc79bdd3c4814bd9ed2
Fowindy <47002339@qq.com>
2020-09-06 16:44:23 +0800

    Listing04_33.使用AND操作符+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_33.使用AND操作符.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 16:41:49
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 16:41:49
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_33
+{
+    [TestClass]
+    public class Listing04_33
+    {
+        [TestMethod]
+        public void Main_Listing04_33()
+        {
+            const string expected =
+                @"请输入现在是几点:<<13
+>>Hi-Ho, Hi-Ho, it's off to work we go.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_33.Listing04_33.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_33.使用AND操作符.cs
@@ -0,0 +1,34 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 16:39:09
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 16:39:09
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_33
+{
+    public class Listing04_33
+    {
+        public static void Main()
+        {
+            Console.Write("请输入现在是几点:");
+            int hourofTheDay = int.Parse(Console.ReadLine());
+            if (10 < hourofTheDay && hourofTheDay < 24)
+            {
+                Console.WriteLine("Hi-Ho, Hi-Ho, it's off to work we go.");
+            }
+        }
+    }
+}
