441dd3ea32d561b2553481f068068e7394989ee8
Fowindy <47002339@qq.com>
2020-09-06 19:58:33 +0800

    Listing04_38.空条件操作符用于索引操作符

---
+++ Src/Chapter04.操作符和控制流程/Listing04_38.空条件操作符用于索引操作符.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月6日 星期日 19:52:39
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月6日 星期日 19:52:39
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_38
+{
+    public class Listing04_38
+    {
+        public static void Main(string[] args)
+        {
+            // CAUTION: args?. Length not verified
+            string directoryPath = args?[0];
+            string searchPattern = args?[1];
+            //．
+        }
+    }
+}
