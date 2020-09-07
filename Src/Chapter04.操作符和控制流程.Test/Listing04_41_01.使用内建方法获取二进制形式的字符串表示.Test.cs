9fdd2fe16aba79c496d2777f36a1d1cdc5ff8bf3
Fowindy <47002339@qq.com>
2020-09-07 13:31:50 +0800

    Listing04_41_01.使用内建方法获取二进制形式的字符串表示+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_41_01.使用内建方法获取二进制形式的字符串表示.Test.cs
@@ -0,0 +1,34 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 13:30:25
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 13:30:25
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_41_01
+{
+    [TestClass]
+    public class Listing04_41_01
+    {
+        [TestMethod]
+        public void Main_Listing04_41_01()
+        {
+            const string expected =
+                @"Please enter an integer:<<13
+>>1101
+15
+d";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_41_01.Listing04_41_01.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_41_01.使用内建方法获取二进制形式的字符串表示.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 11:49:19
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 11:49:19
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_41_01
+{
+    public class Listing04_41_01
+    {
+        public static void Main()
+        {
+            Console.Write("Please enter an integer:");
+            long value = long.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(value, 2));
+            Console.WriteLine(Convert.ToString(value, 8));
+            Console.WriteLine(Convert.ToString(value, 16));
+        }
+    }
+}
