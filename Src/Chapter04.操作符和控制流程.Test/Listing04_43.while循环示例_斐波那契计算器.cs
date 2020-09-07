ef96ae06e6b8faf4eff45992ea8564e276964ace
Fowindy <47002339@qq.com>
2020-09-07 16:14:46 +0800

    Listing04_43.while循环示例_斐波那契计算器+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_43.while循环示例_斐波那契计算器.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 16:01:57
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 16:01:57
+ *          修改理由：使用while循环寻找比输入的数大的第一个斐波那契数
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_43
+{
+    [TestClass]
+    public class Listing04_43
+    {
+        [TestMethod]
+        public void Main_Listing04_43()
+        {
+            const string expected =
+                @"Enter a positive integer:<<8
+>>The Fibonacci number following this is 13";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_43.Listing04_43.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_43.while循环示例_斐波那契计算器.cs
@@ -0,0 +1,44 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 16:00:18
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 16:00:18
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_43
+{
+    public class Listing04_43
+    {
+        public static void Main()
+        {
+            decimal current;
+            decimal previous;
+            decimal temp;
+            decimal input;
+            Console.Write("Enter a positive integer:");
+            input = decimal.Parse(Console.ReadLine());
+            //初始化current和previous为1,斐波那契数列最前两项
+            current = previous = 1;
+            while (current <= input)
+            {
+                temp = current;
+                current = previous + current;
+                previous = temp;
+                //1 1 2 3 5 8 13  
+            }
+            Console.WriteLine($"The Fibonacci number following this is {current}");
+        }
+    }
+}
