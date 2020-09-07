2b4d48c759ea0213e01a5f30725a0f66cd9b43bb
Fowindy <47002339@qq.com>
2020-09-07 13:39:04 +0800

    Listing04_42.使用逻辑赋值操作符+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_42.使用逻辑赋值操作符.Test.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 13:37:35
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 13:37:35
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_42
+{
+    [TestClass]
+    public class Listing04_42
+    {
+        [TestMethod]
+        public void Main_Listing04_42()
+        {
+            const string expected =
+                @"and = 4
+or = 15
+xor = 11";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_42.Listing04_42.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_42.使用逻辑赋值操作符.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 13:34:08
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 13:34:08
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_42
+{
+    public class Listing04_42
+    {
+        public static void Main()
+        {
+            byte and = 12, or = 12, xor = 12;
+            and &= 7;//and = 4
+            or |= 7;//or = 15
+            xor ^= 7;//xor = 11
+            Console.WriteLine($"and = {and}\nor = {or}\nxor = {xor}");
+        }
+    }
+}
