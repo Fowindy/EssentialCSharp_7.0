362ff4a581a8a7e5d02af0062d94a81342ed6eb4
Fowindy <47002339@qq.com>
2020-09-07 17:51:13 +0800

    Listing04_47.使用foreach循环判断剩余走棋+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_47.使用foreach循环判断剩余走棋.Test.cs
@@ -0,0 +1,31 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 17:44:40
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 17:44:40
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_47
+{
+    [TestClass]
+    public class Listing04_47
+    {
+        [TestMethod]
+        public void Main_Listing04_47()
+        {
+            const string expected =
+                @"The available moves are as follows:1,2,3,4,5,6,7,8,9,";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_47.Listing04_47.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_47.使用foreach循环判断剩余走棋.cs
@@ -0,0 +1,37 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 17:44:23
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 17:44:23
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_47
+{
+    public class Listing04_47
+    {
+        public static void Main()
+        {
+            char[] cells = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            Console.Write("The available moves are as follows:");
+            foreach (char cell in cells)
+            {
+                if (cell != '0' && cell != 'X')
+                {
+                    Console.Write($"{cell},");
+                }
+            }
+        }
+    }
+}
