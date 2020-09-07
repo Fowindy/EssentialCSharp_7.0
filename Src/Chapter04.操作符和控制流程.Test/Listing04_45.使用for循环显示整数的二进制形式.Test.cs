3b8dbc281852bb20e30d28fe9b13f2c58e96c0d9
Fowindy <47002339@qq.com>
2020-09-07 16:32:47 +0800

    Listing04_45.使用for循环显示整数的二进制形式+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_45.使用for循环显示整数的二进制形式.Test.cs
@@ -0,0 +1,32 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 16:31:25
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 16:31:25
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_45
+{
+    [TestClass]
+    public class Listing04_45
+    {
+        [TestMethod]
+        public void Main_Listing04_45()
+        {
+            const string expected =
+                @"Please enter an integer:<<-42
+>>1111111111111111111111111111111111111111111111111111111111010110";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_45.Listing04_45.Main);
+        }
+    }
+}
