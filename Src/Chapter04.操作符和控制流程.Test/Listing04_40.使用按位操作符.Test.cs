2368fcc15c2c96c4f5404f1acadb352efd92a2fd
Fowindy <47002339@qq.com>
2020-09-07 09:44:04 +0800

    Listing04_40.使用按位操作符+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_40.使用按位操作符.Test.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 09:43:02
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 09:43:02
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_40
+{
+    [TestClass]
+    public class Listing04_40
+    {
+        [TestMethod]
+        public void Main_Listing04_40()
+        {
+            const string expected =
+                @"and = 4
+or = 15
+xor = 11";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_40.Listing04_40.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_40.使用按位操作符.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 09:39:56
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 09:39:56
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_40
+{
+    public class Listing04_40
+    {
+        public static void Main()
+        {
+            byte and, or, xor;
+            and = 12 & 7;
+            or = 12 | 7;
+            xor = 12 ^ 7;
+            Console.WriteLine($"and = {and}\nor = {or}\nxor = {xor}");
+        }
+    }
+}
