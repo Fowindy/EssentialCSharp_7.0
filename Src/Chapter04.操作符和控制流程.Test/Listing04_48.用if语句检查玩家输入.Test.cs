5705e96baa7ab0f931d84842f56868b4a31e9976
Fowindy <47002339@qq.com>
2020-09-08 10:14:46 +0800

    Listing04_48.用if语句检查玩家输入+测试

---
+++ Src/Chapter04.操作符和控制流程.Test/Listing04_48.用if语句检查玩家输入.Test.cs
@@ -0,0 +1,33 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月8日 星期二 10:13:44
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月8日 星期二 10:13:44
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Test.Listing04_48
+{
+    [TestClass]
+    public class Listing04_48
+    {
+        [TestMethod]
+        public void Main_Listing04_48()
+        {
+            const string expected =
+                @"Please enter move command:<<exit
+>>
+ERROR:Enter a value from 1-9.Push ENTER to quit.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Chapter04.操作符和控制流程.Listing04_48.Listing04_48.Main);
+        }
+    }
+}
---
+++ Src/Chapter04.操作符和控制流程/Listing04_48.用if语句检查玩家输入.cs
@@ -0,0 +1,52 @@
+﻿/* ==============================================================================
+ * 功能描述(Description)：
+ * 
+ * 作者(Author)：Fowindy
+ * 
+ * 日期(Create Date)：2020年9月7日 星期一 21:26:12
+ * 
+ * 修改记录(Revision History)：
+ *      R1：
+ *          修改作者：Fowindy
+ *          修改日期：2020年9月7日 星期一 21:26:12
+ *          修改理由：创建文档
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Chapter04.操作符和控制流程.Listing04_48
+{
+    public class Listing04_48
+    {
+        public static void Main()
+        {
+            Console.Write("Please enter move command:");
+            string input = Console.ReadLine();
+            bool valid = false;
+            if ((input == "1") ||
+                (input == "2") ||
+                (input == "3") ||
+                (input == "4") ||
+                (input == "5") ||
+                (input == "6") ||
+                (input == "7") ||
+                (input == "8") ||
+                (input == "9"))
+            {
+                valid = true;
+            }
+            else if ((input == "") || (input == "quit"))
+            {
+                valid = false;
+            }
+            else
+            {
+                Console.WriteLine("\nERROR:Enter a value from 1-9." +
+                    "Push ENTER to quit.");
+            }
+        }
+    }
+}
